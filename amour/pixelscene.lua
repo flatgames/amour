ae4fbdc7c8eb9b7988fffbb5a8bdd300917b30cb
alexanderdna <alexander.dna@gmail.com>
2018-10-30 17:32:17 +0700

    fix PixelScene

--- amour/pixelscene.lua
+++ amour/pixelscene.lua
@@ -3,25 +3,12 @@ local PixelScene = Node:extend()
 
 function PixelScene:new(designWidth, designHeight)
     local screenWidth, screenHeight = love.graphics.getWidth(), love.graphics.getHeight()
-    local biggerScreenDim = math.max(screenWidth, screenHeight)
-    local biggerDesignDim = math.max(designWidth, designHeight)
+    local sw, sh = screenWidth / designWidth, screenHeight / designHeight
+    local scale = math.floor(math.min(sw, sh))
     
-    local scale = 1
-    while true do
-        local size = biggerDesignDim * scale
-        if size > biggerScreenDim then
-            scale = scale - 1
-            break
-        else
-            scale = scale + 1
-        end
-    end
-
-    if scale == 0 then scale = 1 end
-
     local scaledWidth, scaledHeight = designWidth * scale, designHeight * scale
     local dx, dy = screenWidth - scaledWidth, screenHeight - scaledHeight
-
+    
     local x, y = screenWidth * 0.5, screenHeight * 0.5
     local w, h = designWidth + dx / scale, designHeight + dy / scale
     PixelScene.super.new(self, x, y, w, h)
